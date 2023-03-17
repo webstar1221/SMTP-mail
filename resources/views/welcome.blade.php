875f74a327424202dfb550deee6cf97b605a5040
webstar1221 <djangostars1221@gmail.com>
2023-03-17 21:13:15 +0200

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Swift Email Processor|Cold Email Marketing Software</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,600,600i,700,700i,800,800i,900,900i"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/fontawesome.min.css" />
+    <link rel="stylesheet" href="font/brands.css" />
+    <link rel="stylesheet" href="font/regular.css" />
+    <link rel="stylesheet" href="font/solid.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/responsive.css" />
+  </head>
+
+  <body>
+    <div>
+      <!--header-->
+      <header id="header">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <nav class="navbar navbar-expand-lg navbar-light nav">
+                <a class="navbar-brand logo" href="index.html"
+                  ><img src="images/logo.svg" alt="logo"
+                /></a>
+                <button
+                  class="navbar-toggler"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#navbarNavDropdown"
+                  aria-controls="navbarNavDropdown"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                  <ul class="navbar-nav">
+                    <li class="nav-item active nav-li">
+                      <a class="nav-link nav-a" href="index.html"
+                        >Email Sender and Processor</a
+                      >
+                    </li>
+                    <li class="nav-item active nav-li">
+                      <a
+                        href="/smtpsocks.html"
+                        target="_blank"
+                        class="nav-link nav-a"
+                        >SMTP Socks
+                      </a>
+                    </li>
+                    <li class="nav-item active nav-li">
+                      <a
+                        href="http://webemailverifier.com/"
+                        target="_blank"
+                        class="nav-link nav-a"
+                        >Email Verifier</a
+                      >
+                    </li>
+                    <li class="nav-item active nav-li">
+                      <a
+                        href="/smtp.html"
+                        target="_blank"
+                        class="nav-link nav-a"
+                        >SMTP</a
+                      >
+                    </li>
+                    <li class="nav-item active nav-li">
+                      <a
+                        href="http://swiftsmtp.com/checkout.html"
+                        target="_blank"
+                        class="nav-link nav-a"
+                        >Sign Up</a
+                      >
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
+      <section class="hero-sec">
+        <div class="container">
+        </div>
+      </section>
+
+      <div class="container">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Location</th>
+              <th>Domain/Hostname</th>
+              <th>Hourly Limit</th>
+              <th>Daily Limit</th>
+              <th>Test Send</th>
+              <th>Price</th>
+              <th>Buy</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>111</td>
+              <td>US</td>
+              <td>userfourteenmta.tk</td>
+              <td>None</td>
+              <td>30000</td>
+              <td style="display: flex">
+                <input class="form-control" type="text" />
+                <a class="btn btn-primary">Send</a>
+              </td>
+              <td>$10.00/Month</td>
+              <td>
+                <a href="#">Buy</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <footer id="footer" class="text-center">
+        <div class="container">
+          <p>
+            Swift Email Processor .© 2022 All Rights Reserved<br />
+            All trademarks displayed on this web site are the exclusive property
+            of the respective holders
+          </p>
+        </div>
+      </footer>
+    </div>
+    <!-------Js--------->
+
+    <script src="js/jquery-3.2.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <script>
+
+    </script>
+
+    <!-------Js--------->
+  </body>
+</html>
